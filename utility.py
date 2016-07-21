ce181895c8d2d5bad678eb0f1db4d8e006c4b26a
acesnik <anthony.cesnik@gmail.com>
2016-07-21 16:43:59 -0500

    Small fix to utility.py. Checked validity against previous version of code.

--- utility.py
+++ utility.py
@@ -1,24 +1,26 @@
 # utility methods for summarizeSsdbgResults
 
+import sys
+
 #Read in fasta file
 def read_fasta():
-  try:
-    pepAll = open(sys.argv[2], 'r')
-    allPep = {}
-    header, seq = '',''
-    starting = True
-    count = 0
-    for faLine in pepAll:
-      if len(faLine.split(' ')) == 1: seq += faLine.strip()
-      elif starting:
-        header = faLine.split(' ')[0][1:]
-        starting = False
-      else:
-        allPep[header] = seq
-        count += 1
-        if count % 10000 == 0: print "Read in " + str(count) + " fasta entries"
-        seq = ''
-        header = faLine.split(' ')[0][1:]
-    pepAll.close()
-    return allPep
-  except(IOError): print "IOError"
+    try:
+        pepAll = open(sys.argv[2], 'r')
+        allPep = {}
+        header, seq = '',''
+        starting = True
+        count = 0
+        for faLine in pepAll:
+            if len(faLine.split(' ')) == 1: seq += faLine.strip()
+            elif starting:
+                header = faLine.split(' ')[0][1:]
+                starting = False
+            else:
+                allPep[header] = seq
+                count += 1
+                if count % 10000 == 0: print "Read in " + str(count) + " fasta entries"
+                seq = ''
+                header = faLine.split(' ')[0][1:]
+        pepAll.close()
+        return allPep
+    except IOError: print "IOError"
